04efda6d6ceb4cfb9d94c8f6e5eb42d35688778c
Alper Hasan <alperhasan@gmail.com>
2015-07-10 18:05:36 +0300

    Update card

--- src/Message/UpdateCardRequest.php
+++ src/Message/UpdateCardRequest.php
@@ -24,76 +24,58 @@ class UpdateCardRequest extends AbstractRequest
 {
     public function getData()
     {
-        $this->validate('cardReference');
-        $this->validate('customerReference');
-
         $data = array();
-        $data['description'] = $this->getDescription();
-
-        if ($this->getSource()) {
-            $data['source'] = $this->getSource();
-        } elseif ($this->getToken()) {
-            $data['source'] = $this->getToken();
-        } elseif ($this->getCard()) {
-            $data['source'] = $this->getCardData();
-            $data['email'] = $this->getCard()->getEmail();
-        }
+        $data['id'] = $this->getId();
+        $data['exp_month'] = $this->getExpMonth();
+        $data['exp_year'] = $this->getExpYear();
 
         return $data;
     }
 
     public function getEndpoint()
     {
-        return $this->endpoint . '/customers/' . $this->getCustomerReference() .
-            '/cards/' . $this->getCardReference();
+        return $this->endpoint . '/customers/' . $this->getCustomerId() .
+            '/cards/' . $this->getId();
     }
 
-    /**
-     * Get the card data.
-     *
-     * This request uses a slightly different format for card data to
-     * the other requests and does not require the card data to be
-     * complete in full (or valid).
-     *
-     * @return array
-     */
-    protected function getCardData()
-    {
-        $data = array();
-        $card = $this->getCard();
-        if (! empty($card)) {
-            if ($card->getExpiryMonth()) {
-                $data['exp_month'] = $card->getExpiryMonth();
-            }
-            if ($card->getExpiryYear()) {
-                $data['exp_year'] = $card->getExpiryYear();
-            }
-            if ($card->getName()) {
-                $data['name'] = $card->getName();
-            }
-            if ($card->getNumber()) {
-                $data['number'] = $card->getNumber();
-            }
-            if ($card->getAddress1()) {
-                $data['address_line1'] = $card->getAddress1();
-            }
-            if ($card->getAddress2()) {
-                $data['address_line2'] = $card->getAddress2();
-            }
-            if ($card->getCity()) {
-                $data['address_city'] = $card->getCity();
-            }
-            if ($card->getPostcode()) {
-                $data['address_zip'] = $card->getPostcode();
-            }
-            if ($card->getState()) {
-                $data['address_state'] = $card->getState();
-            }
-            if ($card->getCountry()) {
-                $data['address_country'] = $card->getCountry();
-            }
-        }
 
-        return $data;
+    public function getId()
+    {
+        return $this->getParameter('id');
+    }
+
+    public function setId($value)
+    {
+        return $this->setParameter('id', $value);
+    }
+
+    public function getCustomerId()
+    {
+        return $this->getParameter('customerId');
+    }
+
+    public function setCustomerId($value)
+    {
+        return $this->setParameter('customerId', $value);
+    }
+
+    public function getExpMonth()
+    {
+        return $this->getParameter('expMonth');
+    }
+
+    public function setExpMonth($value)
+    {
+        return $this->setParameter('expMonth', $value);
+    }
+
+    public function getExpYear()
+    {
+        return $this->getParameter('expYear');
+    }
+
+    public function setExpYear($value)
+    {
+        return $this->setParameter('expYear', $value);
     }
 }
